47b75bbeb05dfc2fa18d5c6ebe5b9b1f7a223352
Bob Jacobs <bob.jacobs@outlook.com>
2021-08-13 11:58:00 -0400

    creating web api project

---
+++ web-portal/backend/web-api/src/test/java/com/gpsuscodewith/powerbiembedded/appownsdata/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpsuscodewith.powerbiembedded.appownsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
